f1f84b08ededd46132358a8bf5f38e74ed7102bd
Ronny <60224237+ronny-ellis@users.noreply.github.com>
2025-06-22 11:19:52 +0300

    Update prems

--- gwj-game/script/saussage.gd
+++ gwj-game/script/saussage.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 100
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -200.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 
 func _physics_process(delta: float) -> void:
@@ -12,8 +13,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -34,3 +35,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func player():
+	pass
